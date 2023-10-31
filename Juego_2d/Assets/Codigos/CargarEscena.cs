2b7bb5de41165c16c30b30ebf78e2d9f2fdad63f
edle18 <edwin.18971@gmail.com>
2023-10-31 18:12:02 -0500

    paisajes

---
+++ Juego_2d/Assets/Codigos/CargarEscena.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CargarEscena : MonoBehaviour
+{
+    public void Cargar(string nombre)
+    {
+        StartCoroutine(CargarAsincrona(nombre));
+    }
+
+    IEnumerator CargarAsincrona(string nombre)
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        AsyncOperation operacion = SceneManager.LoadSceneAsync(nombre);
+
+        while (!operacion.isDone)
+        {
+            yield return null;
+        }
+    }
+}
